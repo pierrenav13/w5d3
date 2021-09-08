47db4033b07409f2513aed89b4d333ea2511d266
AdeshPawaroo <adeshpawaroo1@gmail.com>
2021-09-08 10:55:30 -0400

    working on question_follows

---
+++ import_db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows(
+    
+);
